0e67362379a867ee96cb36ceb4574af8376361c0
nicogulo <nicogulobelas@gmail.com>
2021-03-08 16:29:08 +0700

    sneaker-react

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class="Parentbox">
+     <div class="foto">
+     <img src="bwasneaker.jpg"/>
+     </div>
+     <div class="Deskripsi">
+      
+     </div>
+    </div>
+  );
+}
+
+export default App;
