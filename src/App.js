fdb1d075e285f9e5585a9786ef4713c0ab9a9d52
nicogulo <nicogulobelas@gmail.com>
2021-03-09 10:06:38 +0700

    Event handler

--- src/App.js
+++ src/App.js
@@ -7,13 +7,12 @@ function App() {
   return (
     <div className="Parentbox">
       <FotoProduk/>
-      <ProdukInfo category="FUNGKY"/>   
+      <ProdukInfo name="Pingky Dwn" category="FUNGKY"/>   
     </div>
   );
 }
 function FotoProduk(){
   return (
-  
      <div className="Foto">
      <img src="bwasneaker.jpg"/>
      </div>
@@ -23,19 +22,25 @@ function FotoProduk(){
 
 
 function ProdukInfo(props){
-  const {category} = props;
+  const {category, name} = props;
 
   return (
     <div>
      <div className="Deskripsi">
         <p className="Cate">{category}</p>
-        <h1 className="Tittle">Pingky Swag</h1>
+        <h1 className="Tittle">{name}</h1>
         <p className="Price">IDR 2.332.999</p>
         <p className="Info">lasasadadasadafsfsafaksjfbabfuibusbfebfuibaiuuf</p>
+        <a onClick={(e) => TambahCart(name, e)} href="#"> Add to Cart</a>
      </div>
     </div>
   );
 }
+
+function TambahCart(e){
+  return console.log("membeli produk " + e);
+}
+
 export default App;
 
 
